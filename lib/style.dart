3d0564adb6ceebebe0485019b3f9abf5cc33f24c
crazyads69 <leminhtri11a1@gmail.com>
2023-03-08 20:05:34 +0700

    Init MainScreen UI

---
+++ lib/style.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+//Define color code
+const Color babyBlue = Color(0xFF19a4bb);
+const Color lightBlue = Color(0xFF11b3d4);
+
+//Define typography with white color
+const TextStyle wsemiBold = TextStyle(
+    fontFamily: "OpenSans", color: Colors.white, fontWeight: FontWeight.w600);
+const TextStyle wItalic = TextStyle(
+    fontFamily: "OpenSans",
+    color: Colors.white,
+    fontWeight: FontWeight.w300,
+    fontStyle: FontStyle.italic);
+const TextStyle wNormal = TextStyle(
+    fontFamily: "OpenSans", color: Colors.white, fontWeight: FontWeight.w300);
+const TextStyle wRegular = TextStyle(
+    fontFamily: "OpenSans", color: Colors.white, fontWeight: FontWeight.w400);
+
+//Define typography with black color
+const TextStyle blsemiBold = TextStyle(
+    fontFamily: "OpenSans", color: Colors.black, fontWeight: FontWeight.w600);
+const TextStyle blItalic = TextStyle(
+    fontFamily: "OpenSans",
+    color: Colors.black,
+    fontWeight: FontWeight.w300,
+    fontStyle: FontStyle.italic);
+const TextStyle blNormal = TextStyle(
+    fontFamily: "OpenSans", color: Colors.black, fontWeight: FontWeight.w300);
+const TextStyle blRegular = TextStyle(
+    fontFamily: "OpenSans", color: Colors.black, fontWeight: FontWeight.w400);
